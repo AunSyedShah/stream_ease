6d25daa678d323fdc369d12ed2c73ea0cbc93bca
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-08-09 13:04:16 +0500

    add plant component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
-import Image from 'next/image'
-
 export default function Home() {
+  const plants = [
+    {
+      "plant_name":"Neem",
+      "category":"Tree"
+    },
+    {
+      "plant_name":"Neem",
+      "category":"Tree"
+    },
+    {
+      "plant_name":"Neem",
+      "category":"Tree"
+    },
+    {
+      "plant_name":"Neem",
+      "category":"Tree"
+    },
+  ]
   return (
     <div>
       <h1 className='text-blue-400'>Aun Syed Shah</h1>
+      {
+        plants.map(
+          function(plant){
+            return <h1>{plant.plant_name}</h1>
+          }
+        )
+      }
     </div>
   )
 }
