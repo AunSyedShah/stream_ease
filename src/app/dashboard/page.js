46cdc30e8fd8649a97426d5f83844220f544fd08
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2023-08-09 13:11:15 +0500

    bootstrap integration

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,27 @@
-export default function Dashboard(){
-    return(
+'use client';
+
+export default function Dashboard() {
+    return (
         <div>
             <h1>Dashboard</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
